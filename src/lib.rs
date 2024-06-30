43c4c50f2adffb594cb26b1733b56c2b1263c531
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-06-30 17:40:47 +0300

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,9 @@ pub const DEFAULT_LEASE_GRACE_SECONDS: u64 = 5;
 const DEFAULT_RANDOM_IDENTITY_LEN: usize = 32;
 const DEFAULT_FIELD_MANAGER_PREFIX: &str = "kube-lease-";
 
+const DEFAULT_MIN_RANDOM_RELEASE_WAITING_MILLIS: u64 = 100;
+const DEFAULT_MAX_RANDOM_RELEASE_WAITING_MILLIS: u64 = 1000;
+
 /// Represents `kube-lease` specific errors.
 #[derive(thiserror::Error, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum LeaseError {}
@@ -88,15 +91,13 @@ impl Default for LeaseParams {
 
 impl LeaseParams {
     /// Lease lock parameters constructor.
-    pub fn new(
-        identity: impl Into<String>,
-        duration: impl Into<Duration>,
-        grace: impl Into<Duration>,
-    ) -> Self {
+    pub fn new(identity: impl Into<String>, duration: impl Into<Duration>, grace: impl Into<Duration>) -> Self {
         let duration: Duration = duration.into();
         let grace: Duration = grace.into();
 
-        if grace >= duration {
+        if duration == Duration::ZERO || grace == Duration::ZERO {
+            panic!("duration and grace period should be greater than zero");
+        } else if grace >= duration {
             panic!("grace period should be less than lease lock duration");
         }
 
@@ -120,9 +121,7 @@ impl LeaseState {
             api,
             lease_name: lease_name.into(),
             holder: None,
-            expiry: SystemTime::now()
-                .checked_sub(Duration::from_nanos(1))
-                .unwrap(),
+            expiry: SystemTime::now().checked_sub(Duration::from_nanos(1)).unwrap(),
             transitions: 0,
         }
     }
@@ -155,9 +154,7 @@ impl LeaseState {
             self.transitions = lease.lease_transitions.unwrap_or(0);
             self.expiry = {
                 let renew = lease.renew_time;
-                let duration = lease
-                    .lease_duration_seconds
-                    .map(|d| Duration::from_secs(d as u64));
+                let duration = lease.lease_duration_seconds.map(|d| Duration::from_secs(d as u64));
 
                 if renew.is_some() && duration.is_some() {
                     let renew: SystemTime = renew.unwrap().0.into();
@@ -165,9 +162,7 @@ impl LeaseState {
 
                     renew + duration
                 } else {
-                    SystemTime::now()
-                        .checked_sub(Duration::from_nanos(1))
-                        .unwrap()
+                    SystemTime::now().checked_sub(Duration::from_nanos(1)).unwrap()
                 }
             };
         }
@@ -264,14 +259,12 @@ impl LeaseState {
             ..Default::default()
         };
 
-        self.api
-            .patch(&self.lease_name, &params, patch)
-            .await
-            .unwrap();
+        self.api.patch(&self.lease_name, &params, patch).await.unwrap();
 
         Ok(())
     }
 
+    #[allow(dead_code)]
     async fn create(&self) -> Result<Lease> {
         let pp = PostParams::default();
         let data = Lease {
@@ -286,6 +279,7 @@ impl LeaseState {
         Ok(lease)
     }
 
+    #[allow(dead_code)]
     async fn delete(&self) -> Result<()> {
         let dp = DeleteParams::default();
         let _ = self.api.delete(&self.lease_name, &dp).await.unwrap();
@@ -315,28 +309,23 @@ impl LeaseManager {
                 self.sleep_with_grace(self.params.grace).await;
 
                 debug!("renew own lease lock");
-                self.state
-                    .lock(&self.params, LeaseLockOpts::Soft)
-                    .await
-                    .unwrap();
+                self.state.lock(&self.params, LeaseLockOpts::Soft).await.unwrap();
             } else if !self.state.is_locked() {
                 // Lease isn't locket yet
                 debug!("try to lock lease");
-                self.state
-                    .lock(&self.params, LeaseLockOpts::Soft)
-                    .await
-                    .unwrap();
+                self.state.lock(&self.params, LeaseLockOpts::Soft).await.unwrap();
             } else if self.state.is_locked() && self.state.is_expired() {
                 // It's locked by someone else but lock is already expired.
                 // Release it by force and try to lock on the next loop cycle
                 debug!("release expired lease lock");
-                self.state
-                    .release(&self.params, LeaseLockOpts::Force)
-                    .await
-                    .unwrap();
+                self.state.release(&self.params, LeaseLockOpts::Force).await.unwrap();
 
                 // Sleep some random time (up to 500ms) to minimize collisions probability
-                tokio::time::sleep(random_duration(1, 500)).await;
+                tokio::time::sleep(random_duration(
+                    DEFAULT_MIN_RANDOM_RELEASE_WAITING_MILLIS,
+                    DEFAULT_MAX_RANDOM_RELEASE_WAITING_MILLIS,
+                ))
+                .await;
             } else if self.state.is_locked() && !self.state.is_expired() {
                 // It's locked by someone else and lock is actual.
                 // Sleep up to the expiration time of the lock.
@@ -386,3 +375,386 @@ fn random_string(len: usize) -> String {
     rand
 }
 
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+    use k8s_openapi::api::{coordination::v1::LeaseSpec, core::v1::Namespace};
+    use kube::Resource as _;
+    use tokio::sync::OnceCell;
+
+    const TEST_NAMESPACE: &str = "kube-lease-test";
+
+    static INITIALIZED: OnceCell<bool> = OnceCell::const_new();
+
+    async fn init() -> Client {
+        let client = Client::try_default().await.unwrap();
+        INITIALIZED
+            .get_or_init(|| async {
+                create_namespace(client.clone()).await;
+                tracing_subscriber::fmt::init();
+                true
+            })
+            .await;
+
+        client
+    }
+
+    /// Unattended namespace creation
+    async fn create_namespace(client: Client) {
+        let api = Api::<Namespace>::all(client);
+        let pp = PostParams::default();
+
+        let mut data = Namespace::default();
+        data.meta_mut().name = Some(String::from(TEST_NAMESPACE));
+
+        api.create(&pp, &data).await.unwrap_or_default();
+    }
+
+    async fn setup_simple_leaders_vec(lease_name: &str, count: usize) -> (Vec<LeaseParams>, Vec<LeaseState>) {
+        const LEASE_DURATION_SECONDS: u64 = 2;
+        const LEASE_GRACE_SECONDS: u64 = 1;
+
+        let client = init().await;
+        let mut params = vec![];
+        let mut states = vec![];
+
+        for i in 0..count {
+            let param = LeaseParams::new(
+                format!("leader-{i}"),
+                Duration::from_secs(LEASE_DURATION_SECONDS),
+                Duration::from_secs(LEASE_GRACE_SECONDS),
+            );
+            let state = LeaseState::new(client.clone(), lease_name, TEST_NAMESPACE);
+
+            params.push(param);
+            states.push(state);
+        }
+
+        // Create lease
+        let _ = states[0].create().await.unwrap();
+
+        (params, states)
+    }
+
+    #[test]
+    fn lease_params_default_constructor() {
+        let params = LeaseParams::default();
+        assert_eq!(params.identity.len(), DEFAULT_RANDOM_IDENTITY_LEN);
+        assert_eq!(params.duration.as_secs(), DEFAULT_LEASE_DURATION_SECONDS);
+        assert_eq!(params.grace.as_secs(), DEFAULT_LEASE_GRACE_SECONDS);
+    }
+
+    #[test]
+    #[should_panic = "duration and grace period should be greater than zero"]
+    fn incorrect_lease_params_duration_0() {
+        let _params = LeaseParams::new(random_string(10), Duration::from_secs(0), Duration::from_secs(0));
+    }
+
+    #[test]
+    #[should_panic = "duration and grace period should be greater than zero"]
+    fn incorrect_lease_params_grace_0() {
+        let _params = LeaseParams::new(random_string(10), Duration::from_secs(2), Duration::from_secs(0));
+    }
+
+    #[test]
+    #[should_panic = "grace period should be less than lease lock duration"]
+    fn incorrect_lease_params_duration_equal_grace() {
+        let _params = LeaseParams::new(random_string(10), Duration::from_secs(2), Duration::from_secs(2));
+    }
+
+    #[test]
+    #[should_panic = "grace period should be less than lease lock duration"]
+    fn incorrect_lease_params_duration_less_than_grace() {
+        let _params = LeaseParams::new(random_string(10), Duration::from_secs(2), Duration::from_secs(3));
+    }
+
+    #[tokio::test]
+    async fn create_delete() {
+        const LEASE_NAME: &str = "create-delete-test";
+
+        let client = init().await;
+        let state = LeaseState::new(client, LEASE_NAME, TEST_NAMESPACE);
+
+        let lease = state.create().await.unwrap();
+        assert!(lease.spec.is_some());
+        assert_eq!(lease.spec.unwrap(), LeaseSpec::default());
+
+        state.delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn simple_soft_lock_soft_release() {
+        const LEASE_NAME: &str = "simple-soft-lock-soft-release-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 1).await;
+
+        // Lock
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        // Expire
+        tokio::time::sleep(params[0].duration).await;
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(states[0].is_expired());
+
+        // Release
+        states[0].release(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        assert!(!states[0].is_locked());
+        assert!(!states[0].is_holder(&params[0].identity));
+        assert!(states[0].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn soft_lock_1st_soft_release_2nd() {
+        const LEASE_NAME: &str = "soft-lock-1st-soft-release-2nd-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 2).await;
+
+        // Lock by 1st
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Try to release by 2nd, unsuccessfully
+        states[1].release(&params[1], LeaseLockOpts::Soft).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Expire
+        tokio::time::sleep(params[0].duration).await;
+        assert!(states[0].is_expired());
+        assert!(states[1].is_expired());
+
+        // Try to release by 2nd, successfully
+        states[1].release(&params[1], LeaseLockOpts::Soft).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(!states[0].is_locked());
+        assert!(!states[0].is_holder(&params[0].identity));
+        assert!(states[0].is_expired());
+
+        assert!(!states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(!states[1].is_holder(&params[0].identity));
+        assert!(states[1].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn soft_lock_1st_force_release_2nd() {
+        const LEASE_NAME: &str = "soft-lock-1st-force-release-2nd-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 2).await;
+
+        // Lock by 1st
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Try to release by 2nd, successfully
+        states[1].release(&params[1], LeaseLockOpts::Force).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(!states[0].is_locked());
+        assert!(!states[0].is_holder(&params[0].identity));
+        assert!(states[0].is_expired());
+
+        assert!(!states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(!states[1].is_holder(&params[0].identity));
+        assert!(states[1].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn soft_lock_1st_soft_lock_2nd() {
+        const LEASE_NAME: &str = "soft-lock-1st-soft-lock-2nd-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 2).await;
+
+        // Lock by 1st
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Try to lock by 2nd, unsuccessfully
+        states[1].lock(&params[1], LeaseLockOpts::Soft).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Expire
+        tokio::time::sleep(params[0].duration).await;
+        assert!(states[0].is_expired());
+        assert!(states[1].is_expired());
+
+        // Try to lock by 2nd, unsuccessfully
+        states[1].lock(&params[1], LeaseLockOpts::Soft).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(states[1].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn unattended_soft_lock_1st_soft_lock_2nd() {
+        const LEASE_NAME: &str = "unattended-soft-lock-1st-soft-lock-2nd-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 2).await;
+
+        // Lock by 1st and 2nd
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].lock(&params[1], LeaseLockOpts::Soft).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Expire
+        tokio::time::sleep(params[0].duration).await;
+        assert!(states[0].is_expired());
+        assert!(states[1].is_expired());
+
+        // Try to lock by 2nd and 1st
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].lock(&params[1], LeaseLockOpts::Soft).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn unattended_soft_lock_1st_force_lock_2nd() {
+        const LEASE_NAME: &str = "unattended-soft-lock-1st-force-lock-2nd-test";
+        let (params, mut states) = setup_simple_leaders_vec(LEASE_NAME, 2).await;
+
+        // Lock by 1st and 2nd
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].lock(&params[1], LeaseLockOpts::Soft).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(!states[1].is_holder(&params[1].identity));
+        assert!(states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        // Don't expire
+        tokio::time::sleep(params[0].grace).await;
+        assert!(!states[0].is_expired());
+        assert!(!states[1].is_expired());
+
+        // Try to lock by 2nd and 1st
+        states[0].lock(&params[0], LeaseLockOpts::Soft).await.unwrap();
+        states[1].lock(&params[1], LeaseLockOpts::Force).await.unwrap();
+        states[0].sync(LeaseLockOpts::Force).await.unwrap();
+
+        assert!(states[0].is_locked());
+        assert!(!states[0].is_holder(&params[0].identity));
+        assert!(!states[0].is_expired());
+
+        assert!(states[1].is_locked());
+        assert!(states[1].is_holder(&params[1].identity));
+        assert!(!states[1].is_holder(&params[0].identity));
+        assert!(!states[1].is_expired());
+
+        states[0].delete().await.unwrap();
+    }
+
+    #[test]
+    fn random_100_000_8ch_strings() {
+        const SET_LEN: usize = 100_000;
+
+        let mut set = HashSet::new();
+        for _ in 0..SET_LEN {
+            set.insert(random_string(8));
+        }
+
+        assert_eq!(set.len(), SET_LEN);
+    }
+
+    #[test]
+    fn random_100_intervals() {
+        const SET_LEN: usize = 100;
+
+        let mut set = HashSet::new();
+        for _ in 0..SET_LEN {
+            set.insert(random_duration(
+                DEFAULT_MIN_RANDOM_RELEASE_WAITING_MILLIS,
+                DEFAULT_MAX_RANDOM_RELEASE_WAITING_MILLIS,
+            ));
+        }
+
+        assert!(
+            set.len() >= SET_LEN * 8 / 10,
+            "at least 80% of randoms should be unique, but got {}%",
+            set.len()
+        );
+    }
+}
